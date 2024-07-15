71fea2a1d7f7b010a935ac01e280653b7c49fcb3
amergen28 <adem.mergen@ogr.iuc.edu.tr>
2024-07-15 09:51:44 +0300

    added Google Maps

--- lib/government_agency_detail_page.dart
+++ lib/government_agency_detail_page.dart
@@ -1,46 +1,3 @@
-//  import 'package:flutter/material.dart';
-//  import 'models/government_agency.dart';
-
-// class GovernmentAgencyDetailPage extends StatelessWidget {
-//   final GovernmentAgency agency;
-
-//   const GovernmentAgencyDetailPage({Key? key, required this.agency}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Flexible(
-//           child: Text(
-//             agency.title,
-//             style: TextStyle(fontSize: 20),
-//             maxLines: 2,
-//             overflow: TextOverflow.ellipsis,
-//             softWrap: true,
-//           ),
-//         ),
-//       ),
-//       body: Padding(
-//         padding: const EdgeInsets.all(16.0),
-//         child: Column(
-//           crossAxisAlignment: CrossAxisAlignment.start,
-//           children: [
-//             Text('Code: ${agency.code}', style: TextStyle(fontSize: 18)),
-//             SizedBox(height: 8),
-//             Text('Link: ${agency.link}', style: TextStyle(fontSize: 18)),
-//             SizedBox(height: 8),
-//             Text('Tel: ${agency.tel}', style: TextStyle(fontSize: 18)),
-//             SizedBox(height: 8),
-//             Text('Email: ${agency.email}', style: TextStyle(fontSize: 18)),
-//             SizedBox(height: 8),
-//             Text('Address: ${agency.address}', style: TextStyle(fontSize: 18)),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'models/government_agency.dart';
@@ -59,6 +16,21 @@ class GovernmentAgencyDetailPage extends StatelessWidget {
       await launch(launchUri.toString());
     } else {
       throw 'Could not launch $phoneNumber';
+    }
+  }
+
+  Future<void> _openMaps(String address) async {
+    final Uri mapsLaunchUri = Uri(
+      scheme: 'https',
+      host: 'www.google.com',
+      path: 'maps/search/',
+      queryParameters: {'query': address},
+    );
+    final String mapsUrl = 'https://www.google.com/maps/search/?api=1&query=${Uri.encodeFull(address)}';
+    if (await canLaunch(mapsUrl)) {
+      await launch(mapsUrl);
+    } else {
+      throw 'Could not open Maps with $address';
     }
   }
 
@@ -92,13 +64,23 @@ class GovernmentAgencyDetailPage extends StatelessWidget {
               ),
             ),
             SizedBox(height: 8),
-            Text('Email: ${agency.email}', style: TextStyle(fontSize: 18)),
-            SizedBox(height: 8),
-            Text('Address: ${agency.address}', style: TextStyle(fontSize: 18)),
+            GestureDetector(
+              onTap: () => _openMaps(agency.address),
+              child: Text(
+                'Address: ${agency.address}',
+                style: TextStyle(fontSize: 18, color: Colors.blue, decoration: TextDecoration.underline),
+              ),
+            ),
           ],
         ),
       ),
     );
   }
 }
+
+
+
+
+
+
 
